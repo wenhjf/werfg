12b4146a5cd985ccb96db8c42abcd912f4bc332c
wenhjf <135131618+wenhjf@users.noreply.github.com>
2023-05-31 12:59:47 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=cb040952-0451-41fc-8742-bf08df03cb5b
+export UUID=c4c492d2-d916-4f62-846a-50b6fc0158e8
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.thedesignerjackets.com
+export ProxySite=tabletopgamesblog.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
